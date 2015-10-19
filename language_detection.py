54757f02a4a43e830761cda1fd2d65c95c364205
Piplopp <pivertjerome@gmail.com>
2015-10-19 15:58:12 +0200

    upd. comments

--- language_detection.py
+++ language_detection.py
@@ -2,27 +2,19 @@
 
 # -*- coding: utf8 -*-
 
-import sys
-import nltk
-try:
-    from nltk.tokenize import word_tokenize
-    from nltk.corpus import stopwords
-except ImportError:
-    print("no corpora found (~2.4Go) install with: nltk.download()")
-
-# from nltk.tokenize import wordpunct_tokenize
-
-
 def get_language(tokens, stopwords):
     """
         Extract the language from an array of tokens, it is based on stop words
-        and use the nltk corpora as reference
+        It takes two args:
+            tokens    -> list of words to check the language
+            stopwords -> dict of set: {lang : set(language,specific,stop,words)}
 
-        If you don't have the corpora you can install them by nltk.download()
-        and using the id "all", it may take some time to downlaod and ~ 2.4Go
+        It sets a score to a language by intersection between tokens & stopwords
+        by language. The highest score is considered as the main language of
+        the list and is returned.
 
-        improvement: if 2 language have equal score, can look at subject
-        or define a default one
+        improvement: if 2 language have equal score, can look at subject (too
+        small for good detection) or define a default value
     """
 
     languages_ratios = dict()
