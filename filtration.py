205dce22f49cd468cd67eaee5d2f67a8b07d1f6e
Pierre Marijon <pierre@marijon.fr>
2015-10-21 14:16:05 +0200

    Now number is remove

--- filtration.py
+++ filtration.py
@@ -5,15 +5,20 @@
 # import
 import glob
 import os
+import re
+
 
 def filtration(tok_content, stopword, language):
 
     word_ret = list()
 
     for word in tok_content:
-        if word not in stopword.get_stopword()[language] and word not in stopword.get_ponctuation() :
+        if __valid_word(word, stopword, language):
             word_ret.append(word)
-            
+
     return word_ret
 
-          
+
+def __valid_word(word, stopword, language):
+    return word not in stopword.get_stopword()[language] and word not in stopword.get_ponctuation() and not re.match("\d+", word)
+
