9fe573614e2f3ca9a6e738afb7f1af84b541092c
Pierre Marijon <pierre@marijon.fr>
2015-10-22 10:32:20 +0200

    Add some access function to inverted index

--- invertedindex.py
+++ invertedindex.py
@@ -18,3 +18,24 @@ class InvertedIndex:
                     self.index[terms] = list()
                     self.index[terms].append((mail["name"], mail[key][terms]))
 
+    def terms(self):
+        for terms in self.index.keys():
+            yield terms
+
+    def get_terms(self):
+        return self.index.keys()
+
+    def file_counter(self, terms):
+        for val in self.index[terms]:
+            yield val
+
+    def get_file_counter(self, terms):
+        return self.index.values()
+        
+    def file(self, terms):
+        for val in file_counter(terms):
+            yield val[0]
+
+    def counter(self, terms):
+        for val in file_counter(terms):
+            yield val[1]
