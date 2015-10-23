544d0721829cdcc21d0e85d5a9daa407dcf7a1ff
Pierre Marijon <pierre@marijon.fr>
2015-10-23 08:25:14 +0200

    Update comment, and follow pep8 recomendation

--- terms_counter.py
+++ terms_counter.py
@@ -11,10 +11,11 @@ Return a list of simple and complex terms.
 from collections import Counter
 from nltk.util import ngrams
 
+
 ########
 # MAIN #
 ########
-def complexe(list_words, complex_size = 2, threshold = 2):
+def complexe(list_words, complex_size=2, threshold=2):
     """
     This function will analyze a list of words and search for complexe termes.
     Take a list in entry, with the words in the text order.
@@ -23,22 +24,25 @@ def complexe(list_words, complex_size = 2, threshold = 2):
     association_list = []
 
     # For each word in the list, we take the next one and save the combinaison
-    # to make a count on it.
     association_list = ngrams(list_words, complex_size)
 
     # The count of each combinaison will be stocked in a dict
     asso_number = Counter(association_list)
+
+    # Remove combinaison will be not count upper than threshold
     complex_terms = __filter_terms(asso_number, threshold)
 
     return complex_terms
 
 
-def simple(list_words, threshold = 1):
+def simple(list_words, threshold=1):
     """
     This function will analyze a list of words and search for complexe termes.
     Take a list in entry, with the words in the text order.
     Return a dictionary, with simple terms and her occurence.
     """
+
+    # Count all simple terms and remove if isn't upper than threshold
     return __filter_terms(Counter(list_words), threshold)
 
 
@@ -49,23 +53,24 @@ def __filter_terms(dic_combinaison, threshold):
     """
     Take a dictionnary containing each combinaison of termes and return those
     which are found k or more time (default k=2).
-    It return a dict, containing each complex term is a key in string form, 
+    It return a dict, containing each complex term is a key in string form,
     and value is the numbre is presente in dic_combinaison.
     """
     # Remove all key isn't present upper than k, and replace key tuple by a
     # string
-    return {__key2str(key):dic_combinaison[key] for key in
-                    dic_combinaison if dic_combinaison[key] >= threshold}
+    return {__key2str(key): dic_combinaison[key] for key in
+            dic_combinaison if dic_combinaison[key] >= threshold}
 
 
 def __key2str(key):
     """
-    Take a key and return in string format. 
+    Take a key and return in string format.
     """
     if type(key) is tuple:
         return " ".join(key)
     else:
         return key
+
 
 def clean_term_list(list_words, complex_terms):
     """
@@ -81,7 +86,7 @@ def clean_term_list(list_words, complex_terms):
 ########
 if __name__ == "__main__":
     list_of_terms = ["a", "la", "queue", "leu", "leu", "insuffisance",
-                     "cardiaque", "queue", "supermarché", "insuffisance"
-                     , "cardiaque", "diabète", "queue", "leu"]
+                     "cardiaque", "queue", "supermarché", "insuffisance",
+                     "cardiaque", "diabète", "queue", "leu"]
     print(complexe(list_of_terms))
     print(simple(list_of_terms))
