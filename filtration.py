bc2ff9b78c3a55f392ca5fa7b72a793213925530
Nedgang <babarouman@hotmail.fr>
2015-11-10 16:35:04 +0100

    documentation done

--- filtration.py
+++ filtration.py
@@ -1,18 +1,33 @@
 #!/usr/bin/env python3
-
 # -*- coding: utf8 -*-
+"""
+This library allow to filtrate a set of term, deleting non-terms words, using
+a stop words list.
+"""
 
-# import
+##########
+# IMPORT #
+##########
 import glob
 import os
 import re
 
+#############
+# FUNCTIONS #
+#############
 
-def filtration(tok_content, stopword, language):
-
+def filtration(token, stopword, language):
+    """
+    Filtrate each token, depending of a stopword set and the language.
+    Return a list of string.
+    token = a list
+    stopword = stopword object (cf stopwords.py)
+    language = string (french or english)
+    """
     word_ret = list()
 
-    for word in tok_content:
+    for word in token:
+        # Check if the word is in the stopword list
         if __valid_word(word, stopword, language):
             word_ret.append(word)
 
@@ -20,6 +35,13 @@ def filtration(tok_content, stopword, language):
 
 
 def __valid_word(word, stopword, language):
+    """
+    Check if the word is in the stopword list.
+    Return a boolean.
+    word = string
+    stopword = stopword object (cf stopwords.py)
+    language = string (french or english)
+    """
     return (word not in stopword.get_stopword()[language]
             and word not in stopword.get_ponctuation()
             and not re.match("\d+", word))
