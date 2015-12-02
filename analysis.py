368892f1157f3749a64fea2b6af2e3350daa8dcb
Piplopp <pivertjerome@gmail.com>
2015-12-02 17:06:56 +0100

    add keywords selection

--- analysis.py
+++ analysis.py
@@ -54,20 +54,23 @@ def main(arg):
     if arg["print_tag"]:
         print_tag(tagterms)
         return 0
-        
+
     if arg["all_terms"]:
         print_all_terms(tagterms, arg["query"], arg["threshold"])
         return 0
-        
+
     if arg["best_terms"]:
         print_best_terms(tagterms, arg["query"], arg["threshold"])
         return 0
-        
+
     if arg["strict_terms"]:
         print_strict_terms(tagterms, arg["query"], arg["threshold"])
         return 0
-        
-    
+
+    if arg["keywords"]:
+        print_keywords(tagterms, arg["keywords"])
+        return 0
+
 def print_tag(tagterms):
     """ Print all tag in data base """
 
@@ -79,15 +82,15 @@ def print_all_terms(tagterms, query, threshold):
     """ Print all terms is in query tag """
 
     all_terms = dict()
-    
+
     for tag in query:
         all_terms.update(tagterms.get_terms_score(tag))
-        
+
     selected_terms = {k: v for (k,v) in all_terms.items() if v > threshold}
 
     sorted_terms = list()
     [sorted_terms.append((k,v)) for v,k in sorted([(v,k) for k,v in all_terms.items()], reverse=True)]
-    
+
     print(sorted_terms)
 
 
@@ -98,7 +101,7 @@ def print_best_terms(tagterms, query, threshold, bonus=1.5):
 
     for tag in query:
         all_terms.append(tagterms.get_terms_score(tag).keys())
-        
+
     enrich_terms = defaultdict(int)
     for tag in arg["query"]:
         for terms in tagterms.get_terms_score(tag).keys():
@@ -109,13 +112,13 @@ def print_best_terms(tagterms, query, threshold, bonus=1.5):
 
 
     selected_terms = {k: v for (k,v) in enrich_terms.items() if v > threshold}
-                
+
     sorted_terms = list()
     [sorted_terms.append((k,v)) for v,k in sorted([(v,k) for k,v in selected_terms.items()], reverse=True)]
 
     print(sorted_terms)
 
-                
+
 def print_strict_terms(tagterms, query, threshold):
     """ Print just terms is in all tag query """
 
@@ -124,21 +127,35 @@ def print_strict_terms(tagterms, query, threshold):
 
     for tag in query:
         list_of_set.append(set(tagterms.get_terms(tag).keys()))
-        
+
     keep_terms = set.intersection(*list_of_set)
-    
+
     all_terms = dict()
-    
+
     for tag in query:
         all_terms.update(tagterms.get_terms_score(tag))
 
     selected_terms = {k: v for (k,v) in all_terms.items() if k in keep_terms and v > threshold}
-                
+
     sorted_terms = list()
     [sorted_terms.append((k,v)) for v,k in sorted([(v,k) for k,v in selected_terms.items()], reverse=True)]
 
     print(sorted_terms)
-    
+
+
+def print_keywords(tagterms, n):
+    """
+        Print the n first keywords reprensenting the corpus
+    """
+
+    sorted_terms = list()
+    dict_global_terms = tagterms.get_global_score()
+
+    [sorted_terms.append((k,v)) for v,k in sorted([(v,k) for k,v in dict_global_terms.items()], reverse=True)]
+
+    print(sorted_terms[0:n])
+
+
 ##########
 # LAUNCH #
 ##########
--- tag2terms.py
+++ tag2terms.py
@@ -12,11 +12,17 @@ class Tag2Terms:
     def __init__(self):
         self.__tag2terms = defaultdict(lambda: defaultdict(int))
         self.__dfidf = defaultdict(lambda: defaultdict(float))
-        
+        self.__terms_global_counter = defaultdict(int)
+
     def add_mail(self, mail):
         for tag in mail["subject_terms"]:
             for terms in mail["body_terms"].keys():
                 self.__tag2terms[tag][terms] += mail["body_terms"][terms]
+
+        for terms in mail["body_terms"].keys():
+            self.__terms_global_counter[terms] += mail["body_terms"][terms]
+        for terms in mail["subject_terms"]:
+            self.__terms_global_counter[terms] += mail["subject_terms"][terms]
 
     def serialize(self, basename):
         with open(basename+"_count.json", "w") as out:
@@ -24,37 +30,50 @@ class Tag2Terms:
 
         with open(basename+"_score.json", "w") as out:
             json.dump(self.__dfidf, out, ensure_ascii=False)
-            
+
+        with open(basename+"_gcounter.json", "w") as out:
+            json.dump(self.__terms_global_counter, out, ensure_ascii=False)
+
     def read_file(self, basename):
         with open(basename+"_count.json", "r") as infile:
-            self.__tag2terms.update(json.load(infile)) 
+            self.__tag2terms.update(json.load(infile))
 
         with open(basename+"_score.json", "r") as infile:
-            self.__dfidf.update(json.load(infile)) 
-            
+            self.__dfidf.update(json.load(infile))
+
+        with open(basename+"_gcounter.json", "r") as infile:
+            self.__terms_global_counter.update(json.load(infile))
+
     def get_tag(self):
         return self.__tag2terms.keys()
-            
+
     def get_terms(self, tag):
         return self.__tag2terms[tag]
 
     def get_tag_score(self):
         return self.__dfidf.keys()
-            
+
     def get_terms_score(self, tag):
         return self.__dfidf[tag]
-    
+
+    def get_global_score(self):
+        return self.__terms_global_counter
+
     def compute(self):
+        """
+            Compute the score of a term for a given tag
+
+            Score is calculated from the number of occurence of the given term
+            in the corpus and the number of occurence of this term in the
+            subjects of the corpus
+
+            Score is:
+                "the number of occurence of a term IN the tag"
+                -------------------------------------------------
+                "the number of occurence of a term in the corpus"
+        """
         for tag in self.__tag2terms.keys():
             for terms in self.__tag2terms[tag].keys():
-                tf = self.__tag2terms[tag][terms]
-                df = self.__df4terms(terms)
-                self.__dfidf[tag][terms] = math.log(tf/df) * -1
-                
-    def __df4terms(self, terms):
-        ret = 0
-        for tag in self.__tag2terms.keys():
-            if terms in self.__tag2terms[tag]:
-                ret += 1
-
-        return ret
+                tag_occur = self.__tag2terms[tag][terms]
+                global_occur = self.__terms_global_counter[terms]
+                self.__dfidf[tag][terms] = math.log(tag_occur/global_occur) * -1
